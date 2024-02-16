205441c4124f5ea0c4d111493e4c41fef2ed6c21
M. J. San Valero <mariajesus.sanvalero@iesabastos.org>
2024-02-16 13:39:36 +0100

    producto

--- operations.js
+++ operations.js
@@ -7,10 +7,15 @@ var suma = function suma (a, b) {
 var resta = function suma (a, b) {
 	return a - b;	
 };
+/* Función producto */
+var multi = function multi (a, b) {
+	return a * b;	
+};
 
 /* Exportación de funciones */
 module.exports = {
 	suma,
-	resta
+	resta,
+	multi
 }
 
---
+++ test/test3.js
@@ -0,0 +1,18 @@
+// Ejemplo de test utilizando la librería should
+
+// Cargar la librería should
+var should = require('should');
+
+// Cargar el módulo con las funciones para testear
+var operaciones = require('../operations.js');
+
+// Test
+it('comprobar función producto', function() {
+  operaciones.multi.should.be.a.Function();
+  should.equal(operaciones.multi(7,4), 28);
+  should.equal(operaciones.multi(6,3), 18);
+	should.equal(operaciones.multi(1,3), 3);
+});
+
+
+
